9d1e987055c92252eb24f27e075af75be5268041
repoleved08 <geekbii08@gmail.com>
2024-09-08 23:50:09 +0300

    auth with jwt v5

--- controllers/user.go
+++ controllers/user.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/repoleved08/blog/config"
 	"github.com/repoleved08/blog/models"
@@ -20,5 +23,26 @@ func Register(c echo.Context) error {
 }
 
 func Login(c echo.Context) error {
+	var input models.User
+	if err := c.Bind(&input); err.Error != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
+	}
+	var user models.User
+	config.DB.Where("username = ?", input.Username).First(&user)
 
+	if user.ID == 0 || user.CheckPassword(input.Password) != nil {
+		return c.JSON(http.StatusUnauthorized, echo.Map{"error": "invalid credentials"})
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+        "user_id": user.ID,
+        "role":    user.Role,
+        "exp":     time.Now().Add(time.Hour * 72).Unix(),
+    })
+
+    tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+    if err != nil {
+        return err
+    }
+
+    return c.JSON(http.StatusOK, echo.Map{"token": tokenString})
 }
